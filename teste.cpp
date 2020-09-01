afd74b7c735b144f58cf8ce220c39b2ce1aae84f
matheusramosnog96 <matheusramosnog96@users.noreply.github.com>
2020-09-01 01:44:48 -0300

    create teste.cpp

---
+++ teste.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+float soma(float n)
+{return dobra(n)+5;}
+
+float dobra(float n)
+{ return n*2; }
+
+int main()
+{
+       float num-20;
+        cout<<"\nApos cahar a função soma : "<<soma(num);
+         cout<<"\nApos cahar a função dobra : "<<dobra(num);
+         cout<<"\n\n";
+         system("pause";)
+    }
